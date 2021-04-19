16f6e33193bd23dad9f409a92a8369e5c4542a45
Emran Imam <emran.imam@gmail.com>
2021-04-19 14:49:20 +0600

    make it better

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,13 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function create(){
+        return User::create([
+            'name' => 'Emran',
+            'email' => 'emran.imam@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
     }
 }
