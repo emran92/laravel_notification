3a17eb3992ad5c5f578f21f571f576c6503fa57e
Emran Imam <emran.imam@gmail.com>
2021-04-22 15:07:49 +0600

    notify_new

--- app/User.php
+++ app/User.php
@@ -35,4 +35,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Route notifications for the FCM channel.
+     *
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return string
+     */
+    public function routeNotificationForFcm($notification)
+    {
+        return $this->device_token;
+    }
 }
